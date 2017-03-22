821afb641a64decd10cd77e4b40748f2c971676e
Deepak Vishwakarma <deepak.vishwakarma@wwindia.com>
2017-03-22 11:21:14 +0530

    Changes in gitignore as well as validation
    Added upload_reume folder in gitignore, also worked on validation for job description

--- resources/views/frontend/matching_algo/job_description.blade.php
+++ resources/views/frontend/matching_algo/job_description.blade.php
@@ -63,3 +63,22 @@
 
     </div>
 </div>
+
+<script>
+
+    $(document).ready(function ()
+    {
+        $('#candidate_profile_add').validate({
+            errorElement: 'span', //default input error message container
+            errorClass: 'help-block help-block-error', // default input error message class
+            focusInvalid: false, // do not focus the last invalid input
+            ignore: "", // validate all fields including form hidden input
+            rules: {
+                candidate_status:{
+                    required:true,
+                }
+            }
+        });
+    });
+
+</script>
