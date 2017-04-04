74911a4ce4bbac3bd82498c2e60f49ccfebae3f9
Deepak Vishwakarma <deepak.vishwakarma@wwindia.com>
2017-04-04 10:34:21 +0530

    Minor changes in query
    Changes in employer view job query

--- resources/views/employer_frontend/job/view_add_job.blade.php
+++ resources/views/employer_frontend/job/view_add_job.blade.php
@@ -18,19 +18,19 @@
             <tbody id="tbody_filter">
                 @foreach($jobs as $job)
                 <tr>
-                    <td>{{$job['title']}}</td>
-                    <td>{{$job['location']}}</td>
-                    <td>{{$job['created_at']}}</td>
-                    <td>{{$job['cnt_pending_review']}}</td>
-                    <td>{{$job['active_in_interview_phase']}}</td>
-                    <td>{{$job['candidates_not_considered']}}</td>
-                    <td>{{$job['candidates_rejected']}}</td>
+                    <td>{{$job->title}}</td>
+                    <td>{{$job->location}}</td>
+                    <td>{{$job->created_at}}</td>
+                    <td>{{$job->cnt_pending_review}}</td>
+                    <td>{{$job->active_in_interview_phase}}</td>
+                    <td>{{$job->candidates_not_considered}}</td>
+                    <td>{{$job->candidates_rejected}}</td>
                     
                     <td>
-                     @if($job['cnt_pending_review']< 1 && $job['active_in_interview_phase']< 1 && $job['candidates_not_considered']< 1 && $job['candidates_rejected']<1) 
-                        <a href="{{url('edit_job/'.$job['id'])}}">Edit</a>
+                     @if($job->cnt_pending_review< 1 && $job->active_in_interview_phase< 1 && $job->candidates_not_considered < 1 && $job->candidates_rejected<1)
+                        <a href="{{url('edit_job/'.$job->id)}}">Edit</a>
                      @endif
-                       <a href="{{url('view_employer_job/'.$job['id'])}}">view</a> 
+                       <a href="{{url('view_employer_job/'.$job->id)}}">view</a>
                     </td>
                 </tr>
                 @endforeach
