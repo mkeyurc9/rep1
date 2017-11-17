fac4d54426dcf521854b2d4c3f86001e6d645a97
deepak vishwakarma <deepak.vishwakarma@wwindia.com>
2017-11-17 12:51:36 +0530

    paypal credentials

--- config/services.php
+++ config/services.php
@@ -34,5 +34,13 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
+// 'paypal' => [
+//     'client_id' => 'AX6vUWzuI8TbfpxVLDxkJj9wR98Jr69ZyIu5CtLSB8Xh0xVg5Ch3mKLpl5hCw6fAOOtz3S1d1DLhjkHB',
+//     'secret' => 'ENyeHS439UEkXIpq7atwGntc_5jWt_-mA_ubKFURKXdqr7fx7n4QWB7S03DRnmFdG-x1pujsCgWQ-9oo'
 
+// ],
+    'paypal' => [
+    'client_id' => 'AVX82e6y1NbVcQ68-k6kqb4dRW0AlXrRoMaGl_kXdKxkVcU_0PP0p4pQWZjFxB_7ri76PzccU3yq3Aw5',
+    'secret' => 'EGSOdqCdA8s-K4QCbEsRI2zABnHd2YkMaRHsuI7LRDX7Wol64v5UD8rIEtR3ade7u0rf8IrjMCtyJWlP'
+],
 ];
